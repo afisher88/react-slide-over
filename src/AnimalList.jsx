8dbb27382e74cf3c2e4de2420f3c84e85d237fd7
Anthony Fisher <Anthony.Fisher@motors.internal>
2017-12-21 14:51:09 +0000

    Grammar update

--- src/AnimalList.jsx
+++ src/AnimalList.jsx
@@ -4,7 +4,7 @@ import './scss/styles.scss';
 export default class AnimalList extends React.Component {
     render() {
         return(
-            <div>{`Hello you have a list of ${this.props.animals.length} Animals!`}</div>
+            <div>{`Hello, you have a list of ${this.props.animals.length} Animals!`}</div>
         )
     }
 }
